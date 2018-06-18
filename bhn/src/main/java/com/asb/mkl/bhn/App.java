a8dbd49b4f66906e3c107b98f3c5b4d94a7cc0fc
TYSS <TYSS@TYSS-TR-0021>
2018-06-18 16:38:52 +0530

    1st commit

---
+++ bhn/src/main/java/com/asb/mkl/bhn/App.java
@@ -0,0 +1,13 @@
+package com.asb.mkl.bhn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
